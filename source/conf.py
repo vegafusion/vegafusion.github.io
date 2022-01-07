d179921fe86c5d12577d55ff7dc51ae7e86a3b37
Jon Mease <jon.mease.dev@gmail.com>
2022-01-07 14:03:13 -0500

    Add favicon

--- source/conf.py
+++ source/conf.py
@@ -56,6 +56,7 @@ html_theme = "pydata_sphinx_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "_static/VegaFusionLogo-Color.svg"
+html_favicon = "_static/favicon.ico"
 
 # -- Blog configuration ------------------------------------------------------
 blog_post_pattern = "posts/*/*"
